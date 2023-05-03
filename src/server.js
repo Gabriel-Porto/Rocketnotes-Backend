13e07941596c95a0c587f353f4c3de765953ba56
gabriel@desktop <gabrieljoserporto@gmail.com>
2023-05-03 13:57:08 -0300

    Files Structure Config

--- src/server.js
+++ src/server.js
@@ -1,26 +1,11 @@
 const express = require("express"); //IMPORTA EXPRESS
 
-const app = express(); //INSTANCIA EXPRESS
+const routes = require("./routes")
 
+const app = express(); //INSTANCIA EXPRESS
 app.use(express.json()) //ESPECIFICA O FORMATO DOS REQUEST EM JSON
 
-// app.get('/message/:id/:user', (request, response) => {
-//     const { id, user } = request.params;
- 
-//     response.send(`
-//     Mensagem ID: ${id}.
-//     Para o Usuário: ${user}.
-//     `)
-// })
-
-
-app.post("/users", (request, response) => {
-    const { name, email, password } = request.body;
-
-    // response.send(`Usuário: ${name}. E-mail: ${email}. Senha: ${password}`) //SEND: RETORNA NUM FORMATO HTML
-    response.json({ name, email, password });
-})
-
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server Rodando na porta: ${PORT}`));
