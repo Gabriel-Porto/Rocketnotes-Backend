38f5695330d2161a3ec3e59030616803e6444400
gabriel@desktop <gabrieljoserporto@gmail.com>
2023-05-04 11:39:20 -0300

    Modulo Dois Finalizado: Controllers adicionados.

---
+++ src/routes/user.routes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const UserControllers = require("../controllers/UserControllers")
+
+const usersRoutes = Router();
+
+const userControllers = new UserControllers();
+
+// function myMiddleware(request, response, next) {
+//     console.log("Você passou pelo middleware");
+    
+//     if(!request.body.isAdmin) {
+//         return response.status(401).json({ message: "user unauthorized"});
+//     }
+
+//     next();
+// }
+
+usersRoutes.post("/", userControllers.create);
+
+module.exports = usersRoutes;
